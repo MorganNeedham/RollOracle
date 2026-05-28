0f50735319fe19341c2f23a466028ead56bd7c47
MorganNeedham <mhacaldwell@gmail.com>
2026-05-28 16:46:27 -0600

    Add Github Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://morganneedham.github.io/roll-oracle/',
+  base: '/roll-oracle/',
 })
